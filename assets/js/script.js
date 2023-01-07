760b108cf3ecd24fc5a3bddf31af9bd16cfa11e7
ismailaib <ismailaitbouhmad@gmail.com>
2023-01-07 23:41:57 +0100

    add scrollreval

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+const sr = ScrollReveal ({
+    distance: '60px',
+    duration: 2500,
+    reset:true,
+})
+
+
+sr.reveal('.hero-content',{delay:100, origin:'left'})
+sr.reveal('.hero-img',{delay:100, origin:'right'})
+sr.reveal('h5',{delay:100, origin:'bottom'})
+sr.reveal('.location .location-title',{delay:100, origin:'bottom'})
+sr.reveal('.location .location-input',{delay:100, origin:'left'})
+sr.reveal('.card-img',{delay:100, origin:'right'})
+sr.reveal('.card-content',{delay:100, origin:'bottom'})
+sr.reveal('h3',{delay:100, origin:'left'})
+sr.reveal('.contact button',{delay:100, origin:'bottom'})
+sr.reveal('.social',{delay:100, origin:'bottom'})
